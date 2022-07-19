83ebad7a2fa2d87ffb44851fbb29c1d096c4fb6e
HoangDo <hoang.dv@tripi.vn>
2022-07-19 23:39:05 +0700

    feat: Add bean check token

---
+++ src/main/java/vn/nashtech/inventory/user/config/TokenConfig.java
@@ -0,0 +1,20 @@
+package vn.nashtech.inventory.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Configuration
+public class TokenConfig {
+    @Bean
+    public String getTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        // Kiểm tra xem header Authorization có chứa thông tin không
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
